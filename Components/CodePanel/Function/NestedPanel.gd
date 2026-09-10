da6a01852654a4e9bb2990d78e96ab0c600ed2b9
CryingOnion15 <yconnor10@gmail.com>
2026-09-10 11:05:27 -0500

    Starting on nesting

--- Components/CodePanel/Function/NestedPanel.gd
+++ Components/CodePanel/Function/NestedPanel.gd
@@ -1,11 +1,16 @@
 class_name NestedPanel extends FunctionPanel
 
 
+@onready var drop_area: DropArea = $DropArea;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	drop_area.connect("drop_success", on_drop_success);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func on_drop_success():
+	pass;
